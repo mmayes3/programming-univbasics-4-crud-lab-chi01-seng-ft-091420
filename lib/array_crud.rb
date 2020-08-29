61bee1996202095dc9d5dfc5e8a51ccf793e095f
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-29 02:54:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
-  element = "arrays!"
+  element = "wow"
   array.unshift(element)
 end
 

3e08de0be165b4298d514f414ceea37010000f22
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-29 03:05:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,11 +40,13 @@ end
 
 def retrieve_first_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  return arr[0]
   
 end
 
 def retrieve_last_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
   
 end
 
